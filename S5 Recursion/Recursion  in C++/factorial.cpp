1fc35299626c1158aa12d6e986b8885c16d70e3c
SACHIN KUMAR <ersachin.ai@gmail.com>
2026-02-24 23:13:42 +0530

    Factorial Problem Solved

---
+++ S5 Recursion/Recursion  in C++/factorial.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// Recursive function
+int factorial(int n)
+{
+    if (n == 0)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter a positive integer:";
+    cin >> n;
+    cout << "Factorial of " << n << " is " << factorial(n) << endl;
+    return 0;
+}
