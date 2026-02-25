74c7e06f2c48c8994ade9c6db08c9a96c384eca8
SACHIN KUMAR <ersachin.ai@gmail.com>
2026-02-25 23:04:25 +0530

    Solves Palindrome, problem

--- S5 Recursion/Recursion  in C++/palindrome.cpp
+++ S5 Recursion/Recursion  in C++/palindrome.cpp
@@ -1,23 +1,58 @@
+// #include <iostream>
+// using namespace std;
+
+// int reverseNumber(int n, int rev = 0)
+// {
+//     if (n == 0)
+//         return rev;
+//     return reverseNumber(n / 10, rev * 10 + n % 10);
+// }
+
+// int main()
+// {
+//     int n;
+//     cout << "Enter a number: ";
+//     cin >> n;
+
+//     if (n == reverseNumber(n))
+//         cout << "Palindrome Number";
+//     else
+//         cout << "Not a Palindrome";
+
+//     return 0;
+// }
+
 #include <iostream>
+#include <string>
 using namespace std;
 
-int reverseNumber(int n, int rev = 0)
+// Aapka function
+bool isPalindrome(string &str, int start, int end)
 {
-    if (n == 0)
-        return rev;
-    return reverseNumber(n / 10, rev * 10 + n % 10);
+    if (start >= end)
+        return true;
+
+    // Dono characters match hone chahiye AND baki bachi string bhi palindrome honi chahiye
+    return (str[start] == str[end]) && isPalindrome(str, start + 1, end - 1);
 }
 
 int main()
 {
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
+    string word;
+    cout << "Ek word enter karein: ";
+    cin >> word;
 
-    if (n == reverseNumber(n))
-        cout << "Palindrome Number";
+    int n = word.length();
+
+    // Function call yahan hogi
+    if (isPalindrome(word, 0, n - 1))
+    {
+        cout << word << " ek Palindrome hai! ✅" << endl;
+    }
     else
-        cout << "Not a Palindrome";
+    {
+        cout << word << " Palindrome nahi hai. ❌" << endl;
+    }
 
     return 0;
 }
